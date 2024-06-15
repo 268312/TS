b7ad00bf4846c45852126fe9b0f950d2817a252e
268312 <268312@student.pwr.edu.pl>
2024-06-15 13:53:51 +0200

    odkomentowanie

--- src/main/java/com/example/ts/controller/LoginController.java
+++ src/main/java/com/example/ts/controller/LoginController.java
@@ -1,45 +1,45 @@
-//package com.example.ts.controller;
-//
-//import com.example.ts.infrastructure.entity.LoginEntity;
-//import com.example.ts.service.LoginService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-///**
-// * Controller class for handling user login operations.
-// */
-//@RestController
-//@RequestMapping("/auth")
-//public class LoginController {
-//    private final LoginService loginService;
-//
-//    /**
-//     * Constructor for LoginController
-//     * @param loginService The service for handling user login operations
-//     */
-//    @Autowired
-//    public LoginController(LoginService loginService){
-//        this.loginService = loginService;
-//    }
-//
-//    /**
-//     * Logging in
-//     * @param login The login entity containing user credentials
-//     * @return ResponseEntity containing the authentication token if login is successful
-//     */
-//    @PostMapping("/login")
-//    public ResponseEntity<String> login(@RequestBody LoginEntity login){
-//        String token = loginService.userLogin(login);
-//        if(token == null){
-//            return new ResponseEntity<>("Wrong login or password", HttpStatus.UNAUTHORIZED);
-//        } else {
-//            return new ResponseEntity<>(token, HttpStatus.OK);
-//        }
-//    }
-//
-//}
+package com.example.ts.controller;
+
+import com.example.ts.infrastructure.entity.LoginEntity;
+import com.example.ts.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller class for handling user login operations.
+ */
+@RestController
+@RequestMapping("/auth")
+public class LoginController {
+    private final LoginService loginService;
+
+    /**
+     * Constructor for LoginController
+     * @param loginService The service for handling user login operations
+     */
+    @Autowired
+    public LoginController(LoginService loginService){
+        this.loginService = loginService;
+    }
+
+    /**
+     * Logging in
+     * @param login The login entity containing user credentials
+     * @return ResponseEntity containing the authentication token if login is successful
+     */
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginEntity login){
+        String token = loginService.userLogin(login);
+        if(token == null){
+            return new ResponseEntity<>("Wrong login or password", HttpStatus.UNAUTHORIZED);
+        } else {
+            return new ResponseEntity<>(token, HttpStatus.OK);
+        }
+    }
+
+}
--- src/main/java/com/example/ts/security/SecurityConfig.java
+++ src/main/java/com/example/ts/security/SecurityConfig.java
@@ -1,61 +1,61 @@
-//package com.example.ts.security;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    //w bazie danych role zapisuje sie ROLE_NAZWA
-//    //w konfiguracji role zapisuje sie NAZWA
-//    @Value("${jwt.token.key}")
-//    private String key;
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-//        return http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .addFilterBefore(new JWTTokenFilter(key), UsernamePasswordAuthenticationFilter.class)
-//                .authorizeHttpRequests(
-//                        authorizationManagerRequestMatcherRegistry ->
-//                                authorizationManagerRequestMatcherRegistry
-//                                        .requestMatchers("/login").permitAll()     //kazdy moze dojsc do logowania
-//                                        //.requestMatchers("/book").hasRole("USER") //przykład
-//                                        .requestMatchers("/book").permitAll()
-//                )
-//                .sessionManagement(sessionManagement ->
-//                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .build();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();      // kodowanie hasla
-//    }
-//
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
-//                .and()
-//                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
-//    }
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-//        return authenticationConfiguration.getAuthenticationManager();
-//    }
+package com.example.ts.security;
 
-//}
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    //w bazie danych role zapisuje sie ROLE_NAZWA
+    //w konfiguracji role zapisuje sie NAZWA
+    @Value("${jwt.token.key}")
+    private String key;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(new JWTTokenFilter(key), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(
+                        authorizationManagerRequestMatcherRegistry ->
+                                authorizationManagerRequestMatcherRegistry
+                                        .requestMatchers("/login").permitAll()     //kazdy moze dojsc do logowania
+                                        //.requestMatchers("/book").hasRole("USER") //przykład
+                                        .requestMatchers("/book").permitAll()
+                )
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();      // kodowanie hasla
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
